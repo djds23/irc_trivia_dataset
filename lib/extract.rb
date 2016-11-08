a877c55a7f9ed99a5a132240b880ad16f47d9d64
Dean Silfen <dean.silfen@gmail.com>
2016-11-07 20:10:55 -0500

    update extract script and provide outfile

--- lib/extract.rb
+++ lib/extract.rb
@@ -1,6 +1,9 @@
 require 'nokogiri'
 require 'json'
 
+FILEPATH = './raw_html/'
+OUT = './out/'
+
 FILES = [
  "Trivia_Animals.html",
  "Trivia_Food.html",
@@ -19,40 +22,87 @@ FILES = [
 ]
 
 class Question
-  attr_reader :text
+
   def initialize(raw_text)
-    @text = raw_text.strip
+    @text = raw_text
+  end
+
+  def parsed?
+    [question, answer, category].all?
+  end
+
+  def text
+    @text.strip
   end
 
   def question
-    text.split(':').last.split('*').first
+    (text.split(':').last.split('*').first rescue nil)
   end
 
   def answer
-    text.split('*').last
+    (text.split('*').last rescue nil)
   end
 
   def category
-    text.split(':').first
+    (text.split(':').first rescue nil)
   end
 
-  def to_json
+  def to_h
     {
       question: question,
       answer: answer
-    }.to_json
+    }
   end
 end
 
+class Dataset
+  attr_reader :dataset
 
-task :get_open_questions  => :environment  do |t, args|
+  def initialize
+    @dataset = {}
+  end
+
+  def add_to_category(question)
+    questions =
+      if dataset.has_key? question.category
+        dataset[question.category]
+      else
+        []
+      end
+    questions = questions.push(question.to_h)
+    dataset[question.category] = questions
+  end
+
+  def to_json
+    dataset.to_json
+  end
+end
+
+def main
+  dataset = Dataset.new
+  errors = []
+
   FILES.each do |filename|
     raw_text = File.open(FILEPATH + filename).read
     question_nodes = Nokogiri::HTML(raw_text).css('ol').children.children
-    question_nodes.each do |question|
-      clue = text_to_clue(question.text)
-      puts clue.answer
+    question_nodes.each do |question_node|
+      question = Question.new(question_node.text)
+      if question.parsed?
+        dataset.add_to_category(question)
+        puts "parsed question for: #{question.category}"
+      else
+        errors.push(question)
+      end
     end
   end
+
+  errors.each do |error|
+    puts "FAILED TO PARSE: #{error.text}"
+  end
+
+  File.open(OUT + "#{Time.now.to_i}_out.json", 'w') { |file|
+    file.write(dataset.to_json)
+  }
 end
 
+main
